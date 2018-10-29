f186b2861afc6f5d0e22999352466ac5ce9a3d8a
sumitkumar1207 <logsumit28@gmail.com>
2018-10-29 16:12:13 +0530

    done with the quotes and css

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,22 +1,41 @@
 import React, { Component } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import uuid from "uuid";
+
+const data = require("../quotes.json");
 
 class Quotes extends Component {
+  state = {
+    quote: {}
+  };
+  componentDidMount() {
+    this.getQuote();
+  }
+  getQuote = () => {
+    const random = data[Math.floor(Math.random() * data.length)];
+    const quote = random;
+    this.setState({ quote });
+  };
   render() {
     return (
       <div className="App">
         <div className="wrapper">
-          <TransitionGroup component={null}>
-            <CSSTransition classNames="fade">
-              <div className="bubble">
-                <h3 className="quote">
-                  Your quotes will appear here and that will help to others to
-                  get the quotes
-                </h3>
-              </div>
-            </CSSTransition>
-          </TransitionGroup>
-          <button />
+          {this.state.quote.quote && (
+            <TransitionGroup component={null}>
+              <CSSTransition key={uuid()} timeout={1400} classNames="fade">
+                <div className="bubble">
+                  <h1 className="quote">{this.state.quote.quote}</h1>
+                  {this.state.quote.author ? (
+                    <h3> -{this.state.quote.author}</h3>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+
+          <button onClick={() => this.getQuote()} />
         </div>
       </div>
     );
